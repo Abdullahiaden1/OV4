3849f782244aca92ff06f3aaee8a53af1adb3a5e
Pingvu <nhlpool27@gmail.com>
2023-03-17 14:23:11 +0100

    Add project files.

---
+++ OV5/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Tal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double summa = 0;
+
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.Write($"Mata in tal {i}: ");
+                double tal = double.Parse(Console.ReadLine());
+                summa += tal;
+            }
+
+            
+            double medelvarde = summa / 10;
+            Console.WriteLine($"Medelvärdet är {medelvarde}");
+
+            
+            Console.WriteLine("Tryck på en tangent för att avsluta...");
+            
+        }
+    }
+}
